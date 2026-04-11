2647ad1145c4ace3067af28e5a33a9b7787a3da8
Mariam <mariam.gamgoum@gmail.com>
2026-04-11 17:23:14 +0200

    added product detail page for buyer

--- Frontend/src/Approutes.js
+++ Frontend/src/Approutes.js
@@ -1,11 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import OrderView from "./Views/Seller/OrderView";
+import ProductDetail from './Views/Buyer/ProductDetail';
+
+function BlankPage() {
+  return <div style={{ minHeight: '100vh' }} />;
+}
 
 function AppRoutes() {
     return (
         <Router>
             <Routes>
-            <Route path="/" element={<OrderView />} />
+                <Route path="/buyer/home" element={<BlankPage />} />
+                <Route path="/buyer/product/:id" element={<ProductDetail />} />
+                <Route path="/" element={<OrderView />} />
             </Routes>
         </Router>
     );
