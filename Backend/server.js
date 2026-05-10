e60e906625a40f94d3383ebd31b160cc09490b2c
Mohamed Besheer <mbesheer26105@gmail.com>
2026-05-10 04:16:57 +0300

    comment

--- Backend/server.js
+++ Backend/server.js
@@ -22,6 +22,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({ origin: 'https://market-place-demo.vercel.app/', credentials: true }));
+// comment
 app.use(express.json());
 
 // ── API Routes ──────────────────────────────────────────────────────────────
